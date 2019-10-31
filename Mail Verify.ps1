dca884d3b77ea1edf02f2f6ce334aa9064c28356
Artecnet <dominik.metz@de.rhenus.com>
2019-10-31 13:45:08 +0100

    Create Mail Verify.ps1

---
+++ Mail Verify.ps1
@@ -0,0 +1,33 @@
+﻿<#instra
+    Param
+    (
+    [String]
+    $v_MailAddress ='Dominik.metz@de.rhenus.com'
+
+    )
+    #>
+
+    $v_MailAddress ='Dominik.metz@de.rhenus.com'
+
+Function IsValidEmail { 
+    Param ([string] $In) 
+    # Returns true if In is in valid e-mail format. 
+    [system.Text.RegularExpressions.Regex]::IsMatch($In,  
+        '^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)')  
+} 
+
+
+
+
+   IsValidEmail $v_MailAddress
+   
+   test
+   
+   #this is a test to push script via powershell
+   skdgnflksdfmsd
+   sdf
+   sd
+   f
+sdgds
+g
+sdg
